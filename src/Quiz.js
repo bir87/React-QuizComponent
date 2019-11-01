cce3e4536e3a822ebed53c812a7e9d2521729f87
Birbhadra Dhungana <birbhadra.dhungana@arm.com>
2019-11-01 14:05:48 -0500

    ifile  Please enter the commit message for your changes. Lines starting

---
+++ src/Quiz.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component{
+    constructor(props){
+        super(props)
+        this.state = {quiz_position: 1}
+    }
+    render(){
+        return(
+        <div>
+            <div className="QuizQuestion">{quizData.quiz_component[0].instruction_tesxt}
+            </div>
+        </div>
+    )
+    }
+
+
+
+
+}
+
+export default Quiz
