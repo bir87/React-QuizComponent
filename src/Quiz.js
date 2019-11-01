473fb0dd9460a88630cb3ef5dc9628c08bb29c28
Birbhadra Dhungana <birbhadra.dhungana@arm.com>
2019-11-01 14:19:38 -0500

    edit

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,20 +5,13 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component{
     constructor(props){
         super(props)
-        this.state = {quiz_position: 1}
+        this.state = { quiz_position: 1}
     }
     render(){
         return(
-        <div>
-            <div className="QuizQuestion">{quizData.quiz_component[0].instruction_tesxt}
-            </div>
-        </div>
-    )
+            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+        )
     }
-
-
-
-
 }
 
 export default Quiz
